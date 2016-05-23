8c921a85670086ad42bbfd6c0f8d3a4a41d2494d
saroko-dnd <isaroko@mail.ru>
2016-05-23 04:45:19 +0300

    added icon for window

--- CPlusPlus/WinApi/TestsForWindow/CustomIconCursorAndMenu/CustomIconCursorAndMenu/WindowCreation.cpp
+++ CPlusPlus/WinApi/TestsForWindow/CustomIconCursorAndMenu/CustomIconCursorAndMenu/WindowCreation.cpp
@@ -14,8 +14,8 @@ bool WC_RegisterWindowClass(TCHAR NewClassName[], HINSTANCE CurrentApplicationIn
 	WindowStruct.cbWndExtra = NULL;//Почти всегда NULL, за исключением: If an application uses WNDCLASSEX to register a dialog box created by using the CLASS directive in the resource file, 
 								   //it must set this member to DLGWINDOWEXTRA
 	WindowStruct.hInstance = CurrentApplicationInstanceHandler;//Указываем, что оконная процедура находится в этом процессе
-	WindowStruct.hIcon = LoadIcon(NULL, IDI_INFORMATION);//A handle to the class icon. If set to NULL, then it will be default icon.
-	WindowStruct.hIconSm = LoadIcon(NULL, IDI_INFORMATION); //A handle to a small icon that is associated with the window class. If set to NULL then window will have default small icon.
+	WindowStruct.hIcon = LoadIcon(CurrentApplicationInstanceHandler, MAKEINTRESOURCE(FOX_ICON));//Загружаем иконку используя файл ресурсов (MyResources.rc) по его имени (defined in MyResources.h)
+	WindowStruct.hIconSm = LoadIcon(CurrentApplicationInstanceHandler, MAKEINTRESOURCE(FOX_ICON)); //Загружаем иконку используя файл ресурсов (MyResources.rc) по его имени (defined in MyResources.h)
 	WindowStruct.hCursor = LoadCursor(CurrentApplicationInstanceHandler, MAKEINTRESOURCE(CURSOR_GREEN));//Загружаем курсор используя файл ресурсов (MyResources.rc) по его имени (defined in MyResources.h)
 	WindowStruct.hbrBackground = (HBRUSH)GetStockObject(LTGRAY_BRUSH); //A handle to the class background brush. If set to NULL, then background has a color based on current client area settings (default). 
 	WindowStruct.lpszMenuName = NULL;//Pointer to a null-terminated character string that specifies the resource name of the class menu, as the name appears in the resource file.
@@ -42,7 +42,7 @@ HWND WC_CreateWindow(TCHAR ClassName[], HINSTANCE CurrentApplicationInstanceHand
 	return CreateWindowEx(
 		WS_EX_WINDOWEDGE, //The extended window style
 		ClassName,
-		TEXT("Окно со знаком информации"),
+		TEXT("Окно с нестандартными иконкой и курсором"),
 		WS_VISIBLE | WS_SYSMENU | WS_MAXIMIZEBOX | WS_MINIMIZEBOX, //The style of the window. Текущие стили: видимое, кнопка закрыть, кнопка максимально увеличить(уменьшить), кнопка (свернуть).
 		WindowXposition,
 		WindowYposition,
