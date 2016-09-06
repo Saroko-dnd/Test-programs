abaf1516164c8b0c63bbb2d5f7e77ae2128da7b9
saroko-dnd <isaroko@mail.ru>
2016-09-06 17:01:28 +0300

    added code (with one error) for red black tree (C++)

---
+++ JavaScript/TestsOfDataTypes/JSfolder/TestJavaScript.js
@@ -0,0 +1,16 @@
+﻿
+window.onload = TestOfTrueOrFalse;
+
+function TestOfTrueOrFalse()
+{
+    var ResultString = "";
+    var UndefinedVariable;
+    if (!ResultString)
+    {
+        ResultString += "1) Empty string is equal to FALSE.<br>";
+    }
+    if (!UndefinedVariable) {
+        ResultString += "2) Undefined variable is equal to FALSE.<br>";
+    }
+    document.getElementById("PFoResults").innerHTML = ResultString;
+}
